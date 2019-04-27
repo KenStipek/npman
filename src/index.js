d1fb70e048215cb72b10fd7275120fe99ae5238a
Ken Stipek <ken@kenstipek.com>
2019-04-27 13:36:44 -0700

    Add git ignore file and missing files.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// imports
+
+// get arguments
+// https://nodejs.org/docs/latest/api/process.html#process_process_argv
+
+// get content from npm
+
+// display content
+// use ink https://www.npmjs.com/package/ink
+
+console.log('npman')
